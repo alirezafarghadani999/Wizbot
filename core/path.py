ec7d345e4427e519f261c6ab9457c298d2a04295
alirezafarghadani <alirezaf999.iran@gmail.com>
2024-07-04 11:12:43 +0330

    make core.path for saving data

---
+++ core/path.py
@@ -0,0 +1,10 @@
+class PATH:
+    def __init__(self) -> None:
+        self.DataBase = "./public/DataBase/"
+        
+        PATH = {
+            "User_DataBase": self.DataBase + "",
+            "Channel_Database": self.DataBase + "", 
+        }
+
+        return PATH
